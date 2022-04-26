42a827bd6c33b94dc9f37d66fad693fc7cac1917
NikoLindborg <nlindborg@saunassa.com>
2022-04-26 11:19:06 +0300

    added an import

--- iotmgrstart.sh
+++ iotmgrstart.sh
@@ -1,6 +1,7 @@
 //file = iotmgrstart.sh location= /home/iot-device-manager/iotmgrstart.sh
  
  pwd
+ pip install paho-mqtt
  which python3
  python3 /home/pi/iot-device-manager/announcementScript.py
  sleep 2
