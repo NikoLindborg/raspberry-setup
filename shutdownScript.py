e0e355430426437c956368ae0519e1e2f2861887
Niko Lindborg <64002671+NikoLindborg@users.noreply.github.com>
2022-04-26 11:47:39 +0300

    Update shutdownScript.py

--- shutdownScript.py
+++ shutdownScript.py
@@ -7,7 +7,7 @@ from datetime import datetime
 mqttBroker = 'add the ip here'
 client = mqtt.Client('Iot-Device-Manager')
 client.connect(mqttBroker, port=1883, bind_address='')
-file = open('iotid.txt', 'r')
+file = open('/home/pi/iot-device-manager/iotid.txt', 'r')
 deviceId = file.read().splitlines()[0]
 file.close()
 MQTT_MSG=json.dumps({"deviceName": "RASPBERRY 7", "_id": deviceId, "channels":  ["light", "noise"],  "timestamp": datetime.timestamp(datetime.now()), "disconnect": True});
